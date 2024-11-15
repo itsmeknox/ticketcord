3a938b20d3edd7626643a468e1057f12e0443211
Knox <itsmeknox01@gmail.com>
2024-11-15 23:37:11 +0530

    Add ticket_user_required decorator for JWT authentication

---
+++ src/modules/decorator.py
@@ -0,0 +1,31 @@
+
+from functools import wraps
+from flask import request
+from dotenv import load_dotenv
+from utils.exceptions import AuthenticationFailed, InternalServerError
+from utils.validator import TicketUser
+
+from modules.auth import JWT
+
+import os
+
+load_dotenv()
+
+jwt_enc = JWT(encryption_key=os.getenv('CLIENT_SIDE_JWT_TOKEN_SECRET'))
+
+
+def ticket_user_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        token = request.headers.get('Authorization', "")
+        if not token:
+            raise AuthenticationFailed
+
+        user_data = jwt_enc.decrypt(token)
+        try:
+            ticket_user = TicketUser(id=user_data['user_id'], username=user_data['username'], email=user_data['email'], is_authenticated=user_data['is_authenticated'])
+        except KeyError:
+            raise InternalServerError("An unexpected error occurred. Fields are missing in token")
+
+        return func(ticket_user, *args, **kwargs)
+    return wrapper
