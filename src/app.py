654390f7a6a761f135e1103813508cea03cc6cb9
Knox <itsmeknox01@gmail.com>
2024-11-15 23:15:35 +0530

    Add error handlers for AuthenticationFailed and InternalServerError exceptions

--- src/app.py
+++ src/app.py
@@ -5,7 +5,19 @@ app = Flask(__name__)
 
 import routes
 
+from utils.exceptions import InternalServerError, AuthenticationFailed
+
 app.register_blueprint(routes.tickets.bp_tickets)
+
+
+@app.errorhandler(AuthenticationFailed)
+def handle_auth_failed(e: AuthenticationFailed):
+    return e.to_response()
+
+@app.errorhandler(InternalServerError)
+def handle_internal_server_error(e: InternalServerError):
+    return e.to_response()
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
