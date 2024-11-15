abf4c91bd710e31c2939588115f81d70217687c7
Knox <itsmeknox01@gmail.com>
2024-11-15 23:53:34 +0530

    Add get_tickets_by_user_id function to retrieve tickets by user ID and status

--- src/database/tickets.py
+++ src/database/tickets.py
@@ -1,4 +1,5 @@
 from utils.validator import Ticket
+from typing_extensions import List, Literal
 
 
 
@@ -6,4 +7,7 @@ def insert_ticket(data: Ticket):
     ...
 
 def get_ticket(ticket_id: int):
+    ...
+
+def get_tickets_by_user_id(user_id: int, status: List[str]) -> List[Ticket]:
     ...
