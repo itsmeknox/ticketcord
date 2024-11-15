bcbad4875a78d742af533c0d198586484c5fb6d9
Knox <itsmeknox01@gmail.com>
2024-11-15 22:03:34 +0530

    Add ticket model and status type for ticket management

--- utils/validator.py
+++ utils/validator.py
@@ -1,6 +1,19 @@
 from pydantic import BaseModel, EmailStr
 
+from typing_extensions import Literal
 
+
+TICKET_STATUS = Literal["ACTIVE", "CLOSED", "DELETED"]
+
+
+__all__ = (
+    "TICKET_STATUS",
+    "PostTicketPayload",
+    "Ticket",
+)
+
+
+# POST /tickets payload
 class PostTicketUserPayload(BaseModel):
     username: str
     email: EmailStr
@@ -10,3 +23,21 @@ class PostTicketPayload(BaseModel):
     title: str
     description: str
     user: PostTicketUserPayload | None
+
+
+# Ticket model
+class TicketUser(BaseModel):
+    username: str
+    email: EmailStr
+    is_authenticated: bool
+
+class Ticket(BaseModel):
+    id: int
+    title: str
+    description: str
+    user: TicketUser
+    created_at: str
+    updated_at: str
+    seen_message: int | None
+    last_seen_message_id: int | None  # Add this line
+    status: TICKET_STATUSnn
