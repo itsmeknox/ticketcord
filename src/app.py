363f3e62a1f52a652be6c536cb961baab9ae1937
Knox <itsmeknox01@gmail.com>
2024-11-18 14:19:36 +0530

    Refactor app initialization to support threading and environment-based server configuration

--- src/app.py
+++ src/app.py
@@ -6,8 +6,14 @@ import routes.tickets
 from utils.exceptions import InternalServerError, AuthenticationFailed
 from dotenv import load_dotenv
 
+from waitress import serve
+from discord_bot import run_bot
 
+
+import threading
 import routes
+import os
+
 
 load_dotenv()
 app = Flask(__name__)
@@ -24,6 +30,21 @@ def handle_internal_server_error(e: InternalServerError):
     return e.to_response()
 
 
+def run_app():
+    if PORT := os.getenv("WEB_SERVER_PORT") is None:
+        raise ValueError("WEB_SERVER_PORT is not set in the environment variables")
+    
+        
+    if os.getenv("MODE") == "PRODUCTION":
+        serve(app, host="0.0.0.0", port=PORT)
+    else:
+        app.run(host='127.0.0.1', port=PORT, debug=True)
+
+    
+    
+
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    threading.Thread(target=run_bot, daemon=True).start()
+    run_app()
  
