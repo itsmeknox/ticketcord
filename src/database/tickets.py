f991dda912bb31b2a0f685c8a4365f95d3c2b9ec
Knox <itsmeknox01@gmail.com>
2024-11-19 02:53:37 +0530

    Add database interaction for ticket insertion and retrieval

--- src/database/tickets.py
+++ src/database/tickets.py
@@ -1,13 +1,18 @@
-from utils.validator import Ticket
-from typing_extensions import List, Literal
+from utils.validator import Ticket, Message
+from typing_extensions import List
+from utils.mongo_client import get_database
+
+db = get_database()
 
 
 
 def insert_ticket(data: Ticket):
-    ...
+    return db["tickets"].insert_one(data.model_dump())
+
 
 def get_ticket(ticket_id: int):
-    ...
+    return db["tickets"].find_one({"id": ticket_id})
+
 
 def get_tickets_by_user_id(user_id: int, status: List[str]=None) -> List[Ticket]:
     ...
