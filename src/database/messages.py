628ec4593c303053d4217a82f197d4a75397916f
Knox <itsmeknox01@gmail.com>
2024-11-19 02:54:01 +0530

    Add message insertion function to database module

---
+++ src/database/messages.py
@@ -0,0 +1,14 @@
+from utils.mongo_client import get_database
+
+from utils.validator import (
+    Message
+)
+
+db = get_database()['messages']
+
+def insert_message(data: Message):
+    result = db.insert_one(
+        data.model_dump()
+    )
+    return result
+
