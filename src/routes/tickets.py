c2b44c6acabcfa883661c772ee97e12186e47ae5
Knox <itsmeknox01@gmail.com>
2024-11-19 01:47:34 +0530

    Refactor create_ticket function to use run_async_coroutine and add threading lock for ticket creation

--- src/routes/tickets.py
+++ src/routes/tickets.py
@@ -1,22 +1,25 @@
 from flask import Blueprint, request, jsonify
 
-from utils.validator import PostTicketPayload, Ticket, TicketUser
-
-from database.tickets import insert_ticket, get_tickets_by_user_id
+from utils.validator import (
+    PostTicketPayload, 
+    Ticket, 
+    TicketUser
+)
 from modules.decorator import ticket_user_required
-from socket_manager.send_events import send_create_channel_event
-
-
+from discord_bot.app import ticket_manager, run_async_coroutine
 
 from modules.auth import JWT
 from dotenv import load_dotenv
 import os
+import threading
 
 load_dotenv()
 
 
 
-jwt_enc = JWT(encryption_key=os.getenv('SECRET_KEY'))
+
+create_ticket_lock = threading.Lock()
+jwt_enc = JWT(encryption_key=os.getenv('JWT_SECRET_KEY'))
 
 bp_tickets = Blueprint('tickets', __name__, url_prefix='/api/v1/tickets')
 
@@ -28,26 +31,26 @@ def create_ticket(ticket_user: TicketUser):
     ticket_payload = PostTicketPayload.model_validate(request.get_json())
 
     # Create a ticket object
+
+    
+    channel_id, webhook_url = run_async_coroutine(
+        ticket_manager.create_ticket(
+        ticket_payload.title,
+        description=ticket_payload.description,
+        user=ticket_user
+    ))
+
+    
     ticket_data = Ticket(
+        id=channel_id,
         title=ticket_payload.title,
         description=ticket_payload.description,
         user=ticket_user,
         last_seen_message_id=None,
+        webhook_url=webhook_url,
         status="ACTIVE"
     )
 
-    # Need to add the data into database
-    insert_ticket(ticket_data)
 
-    # Need to send an socket event
-    send_create_channel_event(ticket_data)
 
-    return jsonify(ticket_data.model_dump()), 201
-
-@bp_tickets.route('/', methods=['GET'])
-@ticket_user_required
-def get_tickets(ticket_user: TicketUser):
-    
-    tickets = get_tickets_by_user_id(ticket_user.id, status=["ACTIVE", "CLOSED", "PENDING"])
-
-    return jsonify([ticket.model_dump() for ticket in tickets]), 200
+    return jsonify(ticket_data.model_dump(exclude="webhook_url")), 201
