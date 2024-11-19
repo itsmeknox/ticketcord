ea1cab87638e7148b7b396b39c5dac72cc219c71
Knox <itsmeknox01@gmail.com>
2024-11-19 14:28:54 +0530

    Enhance get_ticket function to optionally filter by user_id

--- src/database/tickets.py
+++ src/database/tickets.py
@@ -10,9 +10,12 @@ def insert_ticket(data: Ticket):
     return db["tickets"].insert_one(data.model_dump())
 
 
-def get_ticket(ticket_id: int):
-    return db["tickets"].find_one({"id": ticket_id})
+def get_ticket(ticket_id: int, user_id: int=None):
+    filter = {"id": ticket_id}
+    if user_id:
+        filter["user_id"] = user_id
 
+    return db["tickets"].find_one(filter)
 
 def get_tickets_by_user_id(user_id: int, status: List[str]=None) -> List[Ticket]:
     ...
