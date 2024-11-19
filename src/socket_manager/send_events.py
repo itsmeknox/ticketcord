fdbd1bfbc1a5239ba0854824c163b12a1f6fae4b
Knox <itsmeknox01@gmail.com>
2024-11-19 21:43:23 +0530

    Initialize SocketIO instance and add event emission functions for ticket and message updates

--- src/socket_manager/send_events.py
+++ src/socket_manager/send_events.py
@@ -1,11 +1,21 @@
 from utils.schema import Ticket, Message
+from flask_socketio import SocketIO
 
+sio: SocketIO = None
 
+def socket_sio_init(socket_client: SocketIO):
+    global sio
+    sio = socket_client
 
+def send_create_channel_event(ticket: Ticket):
+    sio.emit("ticket", ticket.model_dump(), room=ticket.user_id)
 
-def send_create_channel_event(data: Ticket):
-    ...
+def ticket_edit_event(ticket: Ticket):
+    sio.emit("edit_ticket", ticket.model_dump(), room=ticket.user_id)
+
     
-    
-def send_message_event(data: Message):
-    ...
+def send_message_event(user_id: int, message: Message):
+    sio.emit("message", message.model_dump(), room=user_id)
+
+def message_edit_event(user_id: int, message: Message):
+    sio.emit("edit_message", message.model_dump(), room=user_id)
