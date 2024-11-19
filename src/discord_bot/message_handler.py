cb6161ad51df583796e90efc0cd6bfb7503f02ba
Knox <itsmeknox01@gmail.com>
2024-11-19 19:12:33 +0530

    Add MessageHandler cog to process messages and link them to tickets

---
+++ src/discord_bot/message_handler.py
@@ -0,0 +1,47 @@
+from discord.ext import commands
+
+from utils.schema import Ticket, Message
+
+from database.tickets import fetch_ticket
+from database.messages import insert_message
+
+from socket_manager.send_events import send_message_event
+
+import asyncio
+import discord
+
+
+
+
+class MessageHandler(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if message.author == self.bot.user:
+            return
+
+        elif message.mentions:
+            print("Mention found returning")
+            return
+        
+        ticket: Ticket = await asyncio.to_thread(fetch_ticket, message.channel.id)
+        if not ticket:
+            print("Ticket not found returning")
+            return
+        
+        message = Message(
+            ticket_id=ticket.id,
+            sender_id=message.author.id,
+            content=message.content,
+            attachments=[attachment.url for attachment in message.attachments]
+        )
+        insert_message(message)
+        send_message_event(message)
+        
+        print("Message sent: ", message.content)
+        
+        
+        
+        
