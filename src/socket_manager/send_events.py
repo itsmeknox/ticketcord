cd67931d2146d9531e34d46f309fac9706c0dc24
Knox <itsmeknox01@gmail.com>
2024-11-19 19:13:30 +0530

    Add dummy send_message_event function to handle message events in socket manager

--- src/socket_manager/send_events.py
+++ src/socket_manager/send_events.py
@@ -1,7 +1,11 @@
-from utils.schema import Ticket
+from utils.schema import Ticket, Message
 
 
 
 
 def send_create_channel_event(data: Ticket):
+    ...
+    
+    
+def send_message_event(data: Message):
     ...
