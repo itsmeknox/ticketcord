5358ab36b37da11355a6cc88ef1cd80291bde115
Knox <itsmeknox01@gmail.com>
2024-11-19 19:26:45 +0530

    Add send_webhook_message function to facilitate sending messages via Discord webhooks

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -1,4 +1,7 @@
 from snowflake import SnowflakeGenerator
+from discord_webhook import DiscordWebhook, DiscordEmbed
+from discord import Embed
+
 import time
 import functools
 
@@ -19,3 +22,23 @@ def calc_timing(func):
         print(f"Func {func.__name__} took {time.time() - start}s to complete")
         return result
     return wrapper
+
+
+
+def send_webhook_message(url: str, content: str, embed: Embed = None, embeds: list[Embed] = None):
+    if embeds and embed:
+        raise ValueError("Cannot have both embed and embeds in the same message")
+    
+    if embed:
+        embeds = [embed]
+    
+    embeds = [embed.to_dict() for embed in embeds]
+    
+    webhook = DiscordWebhook(url=url, content=content, embeds=embeds)
+    
+    try:
+        webhook.execute()
+    except Exception as e:
+        print(f"Error sending webhook message: {e}")
+        return False
+    return True
