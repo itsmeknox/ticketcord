6971614181cf46dd75f48cbff4401fedbeee106d
Knox <itsmeknox01@gmail.com>
2024-11-19 18:52:48 +0530

    Add timing decorator to measure function execution duration

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -1,5 +1,7 @@
 from snowflake import SnowflakeGenerator
 import time
+import functools
+
 gen = SnowflakeGenerator(0)
 
 
@@ -8,3 +10,12 @@ def generate_snowflake_id() -> int:
 
 def generate_timestamp() -> int:
     return int(time.time())
+
+def calc_timing(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        print(f"Func {func.__name__} took {time.time() - start}s to complete")
+        return result
+    return wrapper
