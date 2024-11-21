659e6a68e99a0f7a9f5391f0c6a9b6b0d094fd8b
Knox <itsmeknox01@gmail.com>
2024-11-21 20:07:31 +0530

    Add update and delete message functions to manage ticket messages

--- src/database/messages.py
+++ src/database/messages.py
@@ -18,3 +18,35 @@ def fetch_messages(ticket_id: int) -> list[Message]:
     }
     result = db.find(query)
     return [Message(**data) for data in result]
+
+def update_message_content(ticket_id: str, message_id: str, content: str) -> Message:
+    if not isinstance(content, str):
+        raise ValueError("content must be a string")
+        
+    query = {
+        'id': str(message_id),
+        'ticket_id': str(ticket_id)
+    }
+    data = db.find_one_and_update(
+        query,
+        {'$set': {'content': content}},
+        return_document=True
+        
+    )
+    if data:
+        return Message(**data)
+    
+    return None
+
+
+
+def delete_message(message_id: str):
+    query = {
+        'id': str(message_id)
+    }
+    data = db.find_one_and_delete(query)
+    if data:
+        return Message(**data)
+    return None
+
+        
