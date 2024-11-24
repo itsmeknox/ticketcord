d7503771b7bcbef257c2a3db67e1bee07cd4ebfb
Knox <itsmeknox01@gmail.com>
2024-11-24 21:32:54 +0530

    Refactor socket event functions to use TicketResponse and streamline ticket close event

--- src/socket_manager/send_events.py
+++ src/socket_manager/send_events.py
@@ -1,4 +1,4 @@
-from utils.schema import Ticket, Message
+from utils.schema import TicketResponse, Message
 from flask_socketio import SocketIO
 
 sio: SocketIO = None
@@ -7,12 +7,9 @@ def socket_sio_init(socket_client: SocketIO):
     global sio
     sio = socket_client
 
-def send_create_channel_event(ticket: Ticket):
-    sio.emit("ticket", ticket.model_dump(exclude={"webhook_url"}), room=ticket.user_id)
 
-def ticket_edit_event(ticket: Ticket):
-    sio.emit("edit_ticket", ticket.model_dump(), room=ticket.user_id)
-
+def ticket_close_event(user_id: int, ticket: TicketResponse):
+    sio.emit("ticket_closed", ticket.model_dump(exclude={"webhook_url"}), room=user_id)
     
 def send_message_event(user_id: int, message: Message):
     sio.emit("message", message.model_dump(), room=user_id)
@@ -23,5 +20,3 @@ def message_edit_event(user_id: int, message: Message):
 def message_delete_event(user_id: int, message: Message):
     sio.emit("delete_message", message.model_dump(), room=user_id)
 
-def ticket_close_event(user_id: int, ticket_id: str):
-    sio.emit("ticket_closed", {"id": str(ticket_id)}, room=user_id)
